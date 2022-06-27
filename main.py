7e8e82b1dd3e678dcb9383a7542bf84fa66e2f15
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-27 17:17:09 +0600

    Finalizing.

--- main.py
+++ main.py
@@ -75,14 +75,3 @@ if difference_in_percentage > 3 or difference_in_percentage < 3:
         )
         print(message.status)
         print(article)
-
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
