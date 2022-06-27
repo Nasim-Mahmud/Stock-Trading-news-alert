616a6bacd34f0723f94be23fb82aa51b5bb69338
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-27 16:28:39 +0600

    Initializing Twilio.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 from twilio.rest import Client
 import requests
 
-
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
@@ -11,6 +10,9 @@ STOCK_API = "https://www.alphavantage.co/query"
 
 NEWS_API = "https://newsapi.org/v2/everything"
 NEWS_API_KEY = "505436ebfeac44938deadd3fc0db0d53"
+
+TWILIO_ACCOUNT_SID = "ACc65f7f168ddb0c92bbc38453d0e57978"
+TWILIO_AUTH_TOKEN = "b7724ecc58f0e06dbfb287372512d6a2"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -24,38 +26,40 @@ response.raise_for_status()
 
 stock_data = response.json()
 stock_daily_data = stock_data["Time Series (Daily)"]
-dates  = list(stock_daily_data)
+dates = list(stock_daily_data)
 
 yesterdays_closing_price = stock_daily_data[dates[1]]["4. close"]
 day_before_yesterdays_closing_price = stock_daily_data[dates[2]]["4. close"]
 
-print(yesterdays_closing_price)
-print(day_before_yesterdays_closing_price)
-
 difference = abs(float(yesterdays_closing_price) - float(day_before_yesterdays_closing_price))
 difference_in_percentage = (difference / float(yesterdays_closing_price)) * 100
-print(difference_in_percentage)
+# print(difference_in_percentage)
 
-if difference_in_percentage > 3:
-    print("Get news")
+if difference_in_percentage > 3 or difference_in_percentage < 3:
+    ## STEP 2: Use https://newsapi.org
+    news_parameters = {
+        "apiKey": NEWS_API_KEY,
+        "q": COMPANY_NAME,
+        "sortBy": "popularity",
+        # "category": "business",
+    }
+    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+    news_response = requests.get(NEWS_API, params=news_parameters)
+    news_response.raise_for_status()
 
-
-## STEP 2: Use https://newsapi.org
-news_parameters = {
-    "apiKey": NEWS_API_KEY,
-    "q" : COMPANY_NAME,
-    "sortBy": "popularity",
-    # "category": "business",
-}
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-news_response = requests.get(NEWS_API, params=news_parameters)
-news_response.raise_for_status()
-
-news_data = news_response.json()["articles"]
-print(news_data[0])
-
+    news_data = news_response.json()["articles"]
+    for n in range(0, 3):
+        print(news_data[n]["title"])
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    message = client.messages \
+        .create(
+        body="It may rain today! Bring an umbrella.",
+        from_="[TWILIO_PROVIDED_NUMBER]",  # Got the number from Twilio.
+        to="[PHONE_NUMBER_YOU_REGISTERED_WHILE_CREATING_TWILIO_ACCOUNT]"
+    )
+    print(message.status)
 ## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+# Send a separate message with the percentage change and each article's title and description to your phone number.
 
 
 # Optional: Format the SMS message like this:
